bd7aea0d52edbfba0a7042e80859aaf2f0248d1b
nicbongo <nicbongo@protonmail.com>
2020-05-18 17:08:04 -0300

    iterative_sorting: tests passing

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,23 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
+    # [A, B, C, | Z, Y, X]
+    #  O, O, O, | U, U, U
+    
+    # loop through n-1 elements in arr
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+        cur_index = i # setting current index to variable, neccessary?
+        smallest_index = cur_index # setting index with smallest val to var
 
+        # for every i, loop ahead in same array
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
+        # swapping the arrangement of i and j(smalles_index) if i isn't the smallest
+        if smallest_index is not i:
+            arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
@@ -18,8 +25,10 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    for i in range (0, len(arr) -1 ):
+        for j in range (0, len(arr) -1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 
